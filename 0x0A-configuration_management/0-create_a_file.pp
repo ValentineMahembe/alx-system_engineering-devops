2523a2adcf5eeb7f58ba75308c464cd55db89c47
Valentine Mahembe <mahembevalentine@gmail.com>
2023-11-27 04:32:37 -0800

    0. Create a file correction

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,8 +1,8 @@
 # This Puppet maniffest creates a file at /tmp/school with specific properties
 file { 'tmp/school':
     ensure  => 'file',
-    mode   => '0744',
+    mode    => '0744',
     owner   => 'www-data',
     group   => 'www-data',
-    content   => "I love Puppet\n",
+    content => "I love Puppet\n",
 }
