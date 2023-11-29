bc87c672440fdf4ebc2ee4375597c64d2b681708
Valentine Mahembe <mahembevalentine@gmail.com>
2023-11-29 06:56:52 -0800

    Client configuration file with puppet

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,15 @@
+#Configure ssh using puppet
+
+file_line { 'Declare identity file':
+  path => '/etc/ssh/ssh_config',
+  line => '  IdentityFile ~/.ssh/school',
+  match => '^  IdentityFile',
+  ensure => 'present',
+}
+
+file_line { 'Turn off passwd auth':
+  path => '/etc/ssh/ssh_config',
+  line => '  PasswordAuthentication no',
+  match => '^  PasswordAuthentication',
+  ensure => 'present',
+}
