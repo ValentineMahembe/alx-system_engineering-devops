4656d0edafc915ddda112b55cba2751a3abced43
Valentine Mahembe <mahembevalentine@gmail.com>
2023-12-07 13:34:50 +0200

    Install Nginx web server (w/Puppet)

---
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -0,0 +1,35 @@
+class nginx_install {
+  package { 'nginx':
+    ensure => installed,
+  }
+
+  file { '/var/www/html/index.nginx-debian.html':
+    ensure  => file,
+    content => 'Hello World!',
+  }
+
+  file { '/etc/nginx/sites-available/default':
+    ensure  => file,
+    content => '
+server {
+    listen 80 default_server;
+    listen [::]:80 default_server;
+    root /var/www/html;
+    index index.nginx-debian.html;
+    server_name _;
+    location / {
+        try_files $uri $uri/ =404;
+    }
+    location /redirect_me {
+        return 301 https://www.youtube.com/watch?v=QH2-TGUlwu4;
+    }
+}',
+  }
+
+  service { 'nginx':
+    ensure => running,
+    enable => true,
+  }
+}
+
+include nginx_install
