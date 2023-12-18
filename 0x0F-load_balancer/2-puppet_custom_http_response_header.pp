9234688231da2ad7fd462c4c20131e0cf135a77d
Valentine Mahembe <mahembevalentine@gmail.com>
2023-12-18 15:19:33 +0200

    puppet custom header

---
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -0,0 +1,10 @@
+# Custom HTTP header in an Nginx server
+exec { 'update-and-install-nginx':
+  command  => 'apt-get -y update && apt-get -y install nginx',
+  provider => shell,
+}
+exec { 'configure-nginx':
+  command  => 'sudo sed -i "/listen 80 default_server;/a add_header X-Served-By $HOSTNAME;" /etc/nginx/sites-available/default && service nginx restart',
+  provider => shell,
+  require  => Exec['update-and-install-nginx'],
+}
