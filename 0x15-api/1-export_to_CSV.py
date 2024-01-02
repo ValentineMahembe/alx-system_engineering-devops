64071abd341287c99d837db19d9a0918f6dc4074
Valentine Mahembe <mahembevalentine@gmail.com>
2024-01-02 16:51:42 +0200

    1, 2, 3

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,33 +1,26 @@
 #!/usr/bin/python3
-"""
-Export data to CSV format
-"""
+"""fetches information from JSONplaceholder API and exports to CSV"""
 
-import csv
-import requests
-import sys
+from csv import DictWriter, QUOTE_ALL
+from requests import get
+from sys import argv
+
 
 if __name__ == "__main__":
-    if len(sys.argv) < 2:
-        print('Usage: {} employee_id'.format(sys.argv[0]))
-        sys.exit(1)
+    main_url = "https://jsonplaceholder.typicode.com"
+    todo_url = main_url + "/user/{}/todos".format(argv[1])
+    name_url = main_url + "/users/{}".format(argv[1])
+    todo_result = get(todo_url).json()
+    name_result = get(name_url).json()
 
-    employee_id = sys.argv[1]
-    user_url = "https://jsonplaceholder.typicode.com/users/{}".format(
-            employee_id)
-    todos_url = "https://jsonplaceholder.typicode.com/users/{}/todos".format(
-            employee_id)
-
-    user_response = requests.get(user_url)
-    todos_response = requests.get(todos_url)
-
-    user_data = user_response.json()
-    todos_data = todos_response.json()
-
-    employee_name = user_data.get('name')
-
-    with open('{}.csv'.format(employee_id), 'w', newline='') as csvfile:
-        taskwriter = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
-        for task in todos_data:
-            taskwriter.writerow([employee_id, employee_name,
-                                 task.get('completed'), task.get('title')])
+    todo_list = []
+    for todo in todo_result:
+        todo_dict = {}
+        todo_dict.update({"user_ID": argv[1], "username": name_result.get(
+            "username"), "completed": todo.get("completed"),
+                          "task": todo.get("title")})
+        todo_list.append(todo_dict)
+    with open("{}.csv".format(argv[1]), 'w', newline='') as f:
+        header = ["user_ID", "username", "completed", "task"]
+        writer = DictWriter(f, fieldnames=header, quoting=QUOTE_ALL)
+        writer.writerows(todo_list)
