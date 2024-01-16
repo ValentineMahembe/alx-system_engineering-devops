ebd46457ddb8e0a916976cdfa976ae6e18f3550d
Valentine Mahembe <mahembevalentine@gmail.com>
2024-01-16 19:24:20 +0200

    task #0 correction 3

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -9,12 +9,12 @@ def number_of_subscribers(subreddit):
     """
     Function to return the number of subcribers for a given subreddit.
     """
-    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
     headers = {'User-Agent': 'vmahembe'}
     response = requests.get(url, headers=headers, allow_redirects=False)
 
     if response.status_code == 200:
         data = response.json()
-        return data.get("data").get("subscribers")
+        return data.get('data').get('subscribers')
     else:
         return 0
